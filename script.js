d8aabb1a48e1035960cb5cda8167cbf6203e0275
thebestdev <brandonjinukim@gmail.com>
2026-07-13 19:10:56 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById('colorBtn');
-const title = document.getElementById('main-title');
-
-button.addEventListener('click', () => {
-    title.style.color = 'blue';
-    alert('The color has changed!');
-});
